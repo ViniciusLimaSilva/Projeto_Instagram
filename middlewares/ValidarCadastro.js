5043c4bdd55bf433c27ae7a9220ab8d4c8ff85ea
Vinicius Lima <vini-toninho@hotmail.com>
2021-04-14 14:47:58 -0300

    validação adicionada no código de cadastro através do middleware

---
+++ middlewares/ValidarCadastro.js
@@ -0,0 +1,23 @@
+const { Usuario, sequelize } = require('../models');
+
+module.exports = async (req, res, next) => {
+  let { email, senha, nome } = req.body;
+
+  if (email === null || senha === null || nome === null) {
+    res.status(400).json({ erro: 'Todos os campos devem ser preenchidos ' });
+  } else {
+    let users = await Usuario.findAll({ where: { email } });
+    if (users.length) {
+      res.status(400).json({ erro: 'Email já cadastrado ' });
+      return;
+    } else {
+      if (senha.length < 6 || senha.length > 12) {
+        res.status(400).json({
+          erro: 'Senha deve conter no mínimo 6 dígitos e no máximo 12 ',
+        });
+      } else {
+        next();
+      }
+    }
+  }
+};
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const usuarioscontroller = require('../Controller/UsuariosController');
+const validarcadastro = require('../middlewares/ValidarCadastro');
 
 router.get('/', usuarioscontroller.index);
-router.post('/', usuarioscontroller.create);
+router.post('/', validarcadastro, usuarioscontroller.create);
 router.put('/:id', usuarioscontroller.update);
 router.delete('/:id', usuarioscontroller.delete);
 
